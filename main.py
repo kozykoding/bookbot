61bae5b7fbb04287a3ed3fc3574054227513244f
Sammy <sammy@kozykoding.com>
2025-12-10 11:29:47 -0800

    added to stats and removed filepaths from main to use sys argv

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-from stats import text_count, character_count
+from stats import text_count, character_count, sorted_ch
+import sys
 
-filepath = "./books/frankenstein.txt"
 
-
-def get_book_text(filepath):
-    with open(filepath) as f:
+def get_book_text(book_path):
+    with open(book_path) as f:
         file_contents = f.read()
     return file_contents
 
 
 def main():
-    book = get_book_text(filepath)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    book = get_book_text(book_path)
     count = text_count(book)
     characters = character_count(book)
+    sorted_chars = sorted_ch(book)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {count} total words")
-    print(characters)
+    print("--------- Character Count -------")
+
+    for item in sorted_chars:
+        ch = item["char"]
+        if not ch.isalpha():
+            continue
+        print(f"{ch}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,20 @@ def character_count(file_contents):
         else:
             characters[ch] = 1
     return characters
+
+
+def sort_helper(item):
+    return item["num"]
+
+
+def sorted_ch(file_contents):
+    chars = character_count(file_contents)
+    list_of_dicts = []
+    for ch, count in chars.items():
+        dict = {
+            "char": ch,
+            "num": count,
+        }
+        list_of_dicts.append(dict)
+    list_of_dicts.sort(reverse=True, key=sort_helper)
+    return list_of_dicts
